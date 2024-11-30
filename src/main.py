2fc133826a0a3a15c0cd5d57a3c8cfbde8360058
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 00:47:36 +0200

    included all routes into the main fastapi app

--- src/main.py
+++ src/main.py
@@ -5,12 +5,20 @@ with the different routes of the system.
 """
 
 from fastapi import FastAPI
+from routes.base import base_router
+from routes.data import data_router
+from routes.index import index_router
+from routes.rag import rag_router
 from contextlib import asynccontextmanager
 
 
 @asynccontextmanager
 async def connect_lifespan_clients(app: FastAPI):
-    pass
+    yield
 
 
 app = FastAPI(lifespan=connect_lifespan_clients)
+app.include_router(base_router)
+app.include_router(data_router)
+app.include_router(index_router)
+app.include_router(rag_router)
