b1016f70fbf7f7e32193442cd9751bf1c3b06971
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 03:54:51 +0200

    defined settings object to load .env variables

---
+++ src/helpers/settings_config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    MONGO_ADMIN_USER: str
+    MONGO_ADMIN_PASS: str
+    UNSTRUCTURED_ALLOWED_ASSET_TYPES: list[str]
+
+    class Config:
+        env_file = ".env"
+
+
+def get_settings() -> Settings:
+    return Settings()
