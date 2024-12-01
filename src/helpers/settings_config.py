32b13fd35f8bb9a330245fdfb790d464a1654183
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 04:22:35 +0200

    added asset file chunk size and changed naming scheme

--- src/helpers/settings_config.py
+++ src/helpers/settings_config.py
@@ -4,8 +4,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MONGO_ADMIN_USER: str
     MONGO_ADMIN_PASS: str
-    UNSTRUCTURED_ALLOWED_ASSET_TYPES: list[str]
-    UNSTRUCTURED_ALLOWED_ASSET_SIZE: int
+    ASSET_UNSTRUCTURED_ALLOWED_TYPES: list[str]
+    ASSET_UNSTRUCTURED_ALLOWED_SIZE: int
+    ASSET_UNSTRUCTURED_UPLOAD_CHUNK_SIZE: int
 
     class Config:
         env_file = ".env"
