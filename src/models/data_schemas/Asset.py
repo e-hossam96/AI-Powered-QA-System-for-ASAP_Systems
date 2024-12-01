160fd5e50d0f82bd2683816544c9f27f475e3819
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 05:50:22 +0200

    removed explicit default and left Feild default, allowed arbitrary types to handle ObjectID

--- src/models/data_schemas/Asset.py
+++ src/models/data_schemas/Asset.py
@@ -4,10 +4,13 @@ from pydantic import BaseModel, Field
 
 
 class Asset(BaseModel):
-    id: ObjectId | None = Field(default=None, alias="_id")
+    id: ObjectId = Field(default=None, alias="_id")
     name: str = Field(..., min_length=1)
     type: str = Field(..., min_length=1)
     size: int = Field(gt=0, default=None)
     pushed_at_time: datetime.datetime = Field(
         default=datetime.datetime.now(datetime.timezone.utc)
     )
+
+    class Config:
+        arbitrary_types_allowed = True
