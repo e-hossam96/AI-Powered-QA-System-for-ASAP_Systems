dfac1178c7d5bf1bbc66d300086410112591b3d7
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 04:01:38 +0200

    added allowed max asset size

--- src/helpers/settings_config.py
+++ src/helpers/settings_config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     MONGO_ADMIN_USER: str
     MONGO_ADMIN_PASS: str
     UNSTRUCTURED_ALLOWED_ASSET_TYPES: list[str]
+    UNSTRUCTURED_ALLOWED_ASSET_SIZE: int
 
     class Config:
         env_file = ".env"
