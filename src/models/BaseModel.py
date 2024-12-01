d163f69eda4b4f3addf7bae6f945d7f643c4dfc2
e-hossam96 <ehh.hossam@gmail.com>
2024-12-02 01:18:26 +0200

    added vectordb client to init

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -1,6 +1,8 @@
+from typing import Any
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 
 class BaseModel:
-    def __init__(self, db_client: AsyncIOMotorDatabase) -> None:
+    def __init__(self, db_client: AsyncIOMotorDatabase, vectordb_client: Any) -> None:
         self.db_client = db_client
+        self.vectordb_client = vectordb_client
