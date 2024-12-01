c4acdc6f38a6203625f11919a4af706379036199
e-hossam96 <ehh.hossam@gmail.com>
2024-12-02 01:34:06 +0200

    changed type hints behavior for base model

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -1,4 +1,5 @@
 from typing import Any
+from qdrant_client import QdrantClient
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 
@@ -6,7 +7,7 @@ class BaseModel:
     def __init__(
         self,
         db_client: AsyncIOMotorDatabase | None = None,
-        vectordb_client: Any | None = None,
+        vectordb_client: QdrantClient | Any | None = None,
     ) -> None:
         self.db_client = db_client
         self.vectordb_client = vectordb_client
