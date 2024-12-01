70bb915068a40e6f510d3140c3b1be8ed4924df3
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 03:56:59 +0200

    loaded app settings for all controllers

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,8 +1,10 @@
 import pathlib
+from helpers import Settings
 
 
 class BaseController:
     def __init__(self) -> None:
+        self.app_settings = Settings()
         self.base_dir_path = pathlib.Path(__file__).parent.parent
         self.files_dir_path = self.base_dir_path.joinpath(
             pathlib.Path("assets").joinpath("files")
