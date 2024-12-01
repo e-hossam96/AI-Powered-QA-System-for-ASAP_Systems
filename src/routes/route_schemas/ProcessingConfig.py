3193256b0da9c074251f381cd545275d234fc745
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 23:11:57 +0200

    added processing batch size

--- src/routes/route_schemas/ProcessingConfig.py
+++ src/routes/route_schemas/ProcessingConfig.py
@@ -4,4 +4,5 @@ from pydantic import BaseModel
 class ProcessingConfig(BaseModel):
     chunk_size: int | None = 4000
     overlap_size: int | None = 100
+    batch_size: int | None = 128
     do_reset: bool | None = False
