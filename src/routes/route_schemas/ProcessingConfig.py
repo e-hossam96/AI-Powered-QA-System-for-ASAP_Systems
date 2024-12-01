acb6cc83881c0e52a7871bb728f1a86e6e98449d
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 22:50:46 +0200

    removed asset name -> will always work on all assets

--- src/routes/route_schemas/ProcessingConfig.py
+++ src/routes/route_schemas/ProcessingConfig.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 
 
 class ProcessingConfig(BaseModel):
-    asset_name: str | None = None
     chunk_size: int | None = 4000
     overlap_size: int | None = 100
     do_reset: bool | None = False
