3194761c5feb0b01cb1f1d9cb40d76591071b42b
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 23:21:42 +0200

    added processing responses

--- src/configs/ResponseConfig.py
+++ src/configs/ResponseConfig.py
@@ -6,3 +6,5 @@ class ResponseConfig(Enum):
     ASSET_EXCEEDED_MAX_SIZE: str = "asset exceeded maximum allowed size"
     ASSET_UPLOAD_FAILED: str = "asset upload failed"
     ASSET_UPLOAD_SUCCEEDED: str = "asset upload succeeded"
+    DB_NO_ASSETS: str = "no assets found in database"
+    DB_PROCESSED_ALL_ASSETS: str = "processed all assets into chunks"
