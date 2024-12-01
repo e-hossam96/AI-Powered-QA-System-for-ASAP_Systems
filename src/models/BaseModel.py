e830c23389b6790e4f7a69ba67d5d35157e7d7e8
e-hossam96 <ehh.hossam@gmail.com>
2024-12-02 01:23:24 +0200

    changed bavior of inputs

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -3,6 +3,10 @@ from motor.motor_asyncio import AsyncIOMotorDatabase
 
 
 class BaseModel:
-    def __init__(self, db_client: AsyncIOMotorDatabase, vectordb_client: Any) -> None:
+    def __init__(
+        self,
+        db_client: AsyncIOMotorDatabase | None = None,
+        vectordb_client: Any | None = None,
+    ) -> None:
         self.db_client = db_client
         self.vectordb_client = vectordb_client
