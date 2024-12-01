8d76a128d0baec2c2253d6a24f3248bf3d7582be
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 23:13:09 +0200

    removed batch size

--- src/routes/route_schemas/ProcessingConfig.py
+++ src/routes/route_schemas/ProcessingConfig.py
@@ -4,5 +4,4 @@ from pydantic import BaseModel
 class ProcessingConfig(BaseModel):
     chunk_size: int | None = 4000
     overlap_size: int | None = 100
-    batch_size: int | None = 128
     do_reset: bool | None = False
