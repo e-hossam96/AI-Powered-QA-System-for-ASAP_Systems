189c05c9f852fbd87c254cb958eab7f75b3082a9
e-hossam96 <ehh.hossam@gmail.com>
2024-12-01 05:06:45 +0200

    added mongodb connection url

--- src/helpers/settings_config.py
+++ src/helpers/settings_config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MONGO_ADMIN_USER: str
     MONGO_ADMIN_PASS: str
+    MONGODB_URL: str
+    MONGODB_DATABASE_NAME: str
     ASSET_UNSTRUCTURED_ALLOWED_TYPES: list[str]
     ASSET_UNSTRUCTURED_ALLOWED_SIZE: int
     ASSET_UNSTRUCTURED_UPLOAD_CHUNK_SIZE: int
