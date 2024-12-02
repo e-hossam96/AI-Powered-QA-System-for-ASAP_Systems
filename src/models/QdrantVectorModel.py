380f0753c7d76ecf786fcafc68734ff2b92b3ee3
e-hossam96 <ehh.hossam@gmail.com>
2024-12-02 02:39:32 +0200

    added two methods to push and search vector db

--- src/models/QdrantVectorModel.py
+++ src/models/QdrantVectorModel.py
@@ -1,5 +1,7 @@
 from typing import Any
 from .BaseModel import BaseModel
+from data_schemas import Vector
+from bson.objectid import ObjectId
 from qdrant_client import QdrantClient, models
 
 
@@ -29,3 +31,45 @@ class QdrantVectorModel(BaseModel):
                 ),
             )
         return result
+
+    def batch_push(
+        self,
+        collection_name: str,
+        vectors: list[Vector],
+        batch_size: int = 64,
+    ) -> bool:
+        if not self.vectordb_client.collection_exists(collection_name=collection_name):
+            return False
+        metadata = [v.model_dump(mode="json", exclude_none=True) for v in vectors]
+        vectors = [m.pop("vector") for m in metadata]
+        self.vectordb_client.upload_collection(
+            collection_name=collection_name,
+            vectors=vectors,
+            payload=metadata,
+            batch_size=batch_size,
+        )
+        return True
+
+    def search_by_vector(
+        self, collection_name: str, vector: Vector, limit: int = 4
+    ) -> list[Vector]:
+        records = []
+        if self.vectordb_client.collection_exists(collection_name=collection_name):
+            result = self.vectordb_client.search(
+                collection_name=collection_name,
+                query_vector=vector,
+                limit=limit,
+            )
+        if result is not None:
+            records.extend(
+                [
+                    Vector(
+                        text=record.payload["text"],
+                        source_name=record.payload["source_name"],
+                        source_id=ObjectId(record.payload["source_id"]),
+                        score=record.score,
+                    )
+                    for record in result
+                ]
+            )
+        return records
