2a2fc72238fe5ccaee287f09851e12f815d2cd33
e-hossam96 <ehh.hossam@gmail.com>
2024-12-02 04:08:12 +0200

    added embedding and generation clients to base model

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -1,4 +1,5 @@
 from typing import Any
+from openai import AsyncOpenAI
 from qdrant_client import AsyncQdrantClient
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
@@ -7,7 +8,11 @@ class BaseModel:
     def __init__(
         self,
         db_client: AsyncIOMotorDatabase | None = None,
-        vectordb_client: AsyncQdrantClient | Any | None = None,
+        vectordb_client: AsyncQdrantClient | None = None,
+        embedding_client: AsyncOpenAI | None = None,
+        generation_client: AsyncOpenAI | None = None,
     ) -> None:
         self.db_client = db_client
         self.vectordb_client = vectordb_client
+        self.embedding_client = embedding_client
+        self.generation_client = generation_client
