34a734c819f45f5d686432da3b73ec2fb1d4a737
Hossam Gamal Mostafa <ehh.hossam@gmail.com>
2025-01-18 21:13:44 +0200

    removed sensitive data from inputs

--- src/evaluation/eval_completeness.py
+++ src/evaluation/eval_completeness.py
@@ -2,27 +2,28 @@ import json
 import weave
 from models import OpenAILLMModel
 from configs import OpenAIRoleConfig
-from helpers.settings_config import Settings
 from locales.eval_completeness_templates import SYSTEM_PROMPT, USER_PROMPT
 
 
 @weave.op()
 async def score_completeness(
-    query: str, response: str, app_settings: Settings, eval_model: OpenAILLMModel
+    query: str, response: str, eval_model: OpenAILLMModel, **kwargs
 ) -> dict | None:
     messages = [
-        {OpenAIRoleConfig.SYSTEM.value: SYSTEM_PROMPT.safe_substitute()},
         {
-            OpenAIRoleConfig.USER.value: USER_PROMPT.safe_substitute(
-                user_query=query, generated_answer=response
-            )
+            "role": OpenAIRoleConfig.SYSTEM.value,
+            "content": SYSTEM_PROMPT.substitute({}),
+        },
+        {
+            "role": OpenAIRoleConfig.USER.value,
+            "content": USER_PROMPT.substitute(
+                {"user_query": query, "generated_answer": response}
+            ),
         },
     ]
     ans = await eval_model.generate_text(
-        model_name=app_settings.EVAL_LLM_MODEL_NAME,
         messages=messages,
-        max_output_tokens=app_settings.EVAL_LLM_MAX_OUTPUT_TOKENS,
-        temperature=app_settings.EVAL_LLM_TEMPERATURE,
+        **kwargs,
     )
     if ans is None:
         return None
