f7b4e15f7b8b30d102ebdba22b9b3b9434904d9c
Hossam Gamal Mostafa <ehh.hossam@gmail.com>
2025-01-18 23:50:02 +0200

    split eval settings into a separate obj for safety

--- src/routes/eval.py
+++ src/routes/eval.py
@@ -8,26 +8,52 @@ from evaluation.eval_completeness import score_completeness
 from evaluation.rag_pipelines import ToolCallRAGPipeline, RAGSettings
 from evaluation.utils import read_eval_data
 from models import OpenAILLMModel, QdrantVectorModel
+from dataclasses import dataclass
 
 eval_router = APIRouter(prefix="/rag", tags=["rag", "evaluation"])
 
 
+@dataclass
+class EvaluationSettings:
+    EVAL_LLM_BASE_URL: str
+    EVAL_LLM_MODEL_NAME: str
+    EVAL_LLM_SPECIAL_TOKENS: list[str]
+    EVAL_LLM_MAX_PROMPT_TOKENS: int
+    EVAL_LLM_MAX_OUTPUT_TOKENS: int
+    EVAL_LLM_TEMPERATURE: float
+    EVAL_DATA_NAME: str
+    EVAL_DATA_PATH: str
+
+
+# split settings for security
+app_settings = get_settings()
+rag_settings = RAGSettings(
+    GENERATION_LLM_BASE_URL=app_settings.GENERATION_LLM_BASE_URL,
+    GENERATION_LLM_MODEL_NAME=app_settings.GENERATION_LLM_MODEL_NAME,
+    GENERATION_LLM_SPECIAL_TOKENS=app_settings.GENERATION_LLM_SPECIAL_TOKENS,
+    GENERATION_LLM_MAX_PROMPT_TOKENS=app_settings.GENERATION_LLM_MAX_PROMPT_TOKENS,
+    GENERATION_LLM_MAX_OUTPUT_TOKENS=app_settings.GENERATION_LLM_MAX_OUTPUT_TOKENS,
+    GENERATION_LLM_TEMPERATURE=app_settings.GENERATION_LLM_TEMPERATURE,
+    EMBEDDING_LLM_BASE_URL=app_settings.EMBEDDING_LLM_BASE_URL,
+    EMBEDDING_LLM_MODEL_NAME=app_settings.EMBEDDING_LLM_MODEL_NAME,
+    EMBEDDING_LLM_SPECIAL_TOKENS=app_settings.EMBEDDING_LLM_SPECIAL_TOKENS,
+    EMBEDDING_LLM_EMBEDDING_SIZE=app_settings.EMBEDDING_LLM_EMBEDDING_SIZE,
+    EMBEDDING_LLM_MAX_PROMPT_TOKENS=app_settings.EMBEDDING_LLM_MAX_PROMPT_TOKENS,
+)
+eval_settings = EvaluationSettings(
+    EVAL_LLM_BASE_URL=app_settings.EVAL_LLM_BASE_URL,
+    EVAL_LLM_MODEL_NAME=app_settings.EVAL_LLM_MODEL_NAME,
+    EVAL_LLM_SPECIAL_TOKENS=app_settings.EVAL_LLM_SPECIAL_TOKENS,
+    EVAL_LLM_MAX_PROMPT_TOKENS=app_settings.EVAL_LLM_MAX_PROMPT_TOKENS,
+    EVAL_LLM_MAX_OUTPUT_TOKENS=app_settings.EVAL_LLM_MAX_OUTPUT_TOKENS,
+    EVAL_LLM_TEMPERATURE=app_settings.EVAL_LLM_TEMPERATURE,
+    EVAL_DATA_NAME=app_settings.EVAL_DATA_NAME,
+    EVAL_DATA_PATH=app_settings.EVAL_DATA_PATH,
+)
+
+
 @eval_router.get("/evaluate")
 async def evaluate(request: Request) -> JSONResponse:
-    app_settings = get_settings()
-    rag_settings = RAGSettings(
-        GENERATION_LLM_BASE_URL=app_settings.GENERATION_LLM_BASE_URL,
-        GENERATION_LLM_MODEL_NAME=app_settings.GENERATION_LLM_MODEL_NAME,
-        GENERATION_LLM_SPECIAL_TOKENS=app_settings.GENERATION_LLM_SPECIAL_TOKENS,
-        GENERATION_LLM_MAX_PROMPT_TOKENS=app_settings.GENERATION_LLM_MAX_PROMPT_TOKENS,
-        GENERATION_LLM_MAX_OUTPUT_TOKENS=app_settings.GENERATION_LLM_MAX_OUTPUT_TOKENS,
-        GENERATION_LLM_TEMPERATURE=app_settings.GENERATION_LLM_TEMPERATURE,
-        EMBEDDING_LLM_BASE_URL=app_settings.EMBEDDING_LLM_BASE_URL,
-        EMBEDDING_LLM_MODEL_NAME=app_settings.EMBEDDING_LLM_MODEL_NAME,
-        EMBEDDING_LLM_SPECIAL_TOKENS=app_settings.EMBEDDING_LLM_SPECIAL_TOKENS,
-        EMBEDDING_LLM_EMBEDDING_SIZE=app_settings.EMBEDDING_LLM_EMBEDDING_SIZE,
-        EMBEDDING_LLM_MAX_PROMPT_TOKENS=app_settings.EMBEDDING_LLM_MAX_PROMPT_TOKENS,
-    )
     vectordb_model = QdrantVectorModel(request.app.vectordb_client)
     embedding_model = OpenAILLMModel(embedding_client=request.app.embedding_client)
     generation_model = OpenAILLMModel(generation_client=request.app.generation_client)
@@ -41,16 +67,16 @@ async def evaluate(request: Request) -> JSONResponse:
         generation_controller=generation_controller,
         rag_settings=rag_settings,
     )
-    eval_dataset = read_eval_data(Path(app_settings.EVAL_DATA_PATH))
+    eval_dataset = read_eval_data(Path(eval_settings.EVAL_DATA_PATH))
 
     async def scorer(query, output):
         return await score_completeness(
             query,
             output["response"],
             evaluation_model,
-            model_name=app_settings.EVAL_LLM_MODEL_NAME,
-            max_output_tokens=app_settings.EVAL_LLM_MAX_OUTPUT_TOKENS,
-            temperature=app_settings.EVAL_LLM_TEMPERATURE,
+            model_name=eval_settings.EVAL_LLM_MODEL_NAME,
+            max_output_tokens=eval_settings.EVAL_LLM_MAX_OUTPUT_TOKENS,
+            temperature=eval_settings.EVAL_LLM_TEMPERATURE,
         )
 
     completeness_evaluator = weave.Evaluation(
